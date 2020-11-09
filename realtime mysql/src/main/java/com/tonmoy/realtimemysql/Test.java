dab3b6006e745082e563b8a645bbf8a987f50770
Tonmoy <andro.dev.hub@gmail.com>
2020-11-09 16:28:50 +0600

    fghgfh

---
+++ realtime mysql/src/main/java/com/tonmoy/realtimemysql/Test.java
@@ -0,0 +1,15 @@
+package com.tonmoy.realtimemysql;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
