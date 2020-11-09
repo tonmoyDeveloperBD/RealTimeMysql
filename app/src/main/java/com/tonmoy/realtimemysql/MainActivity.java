afc8e8100ffb8271a0a301d0c808998ef98746dc
Tonmoy <andro.dev.hub@gmail.com>
2020-11-09 16:48:24 +0600

    test

--- app/src/main/java/com/tonmoy/realtimemysql/MainActivity.java
+++ app/src/main/java/com/tonmoy/realtimemysql/MainActivity.java
@@ -3,6 +3,8 @@ package com.tonmoy.realtimemysql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.tonmoy.realtimemysql.MainActivity;
+import com.tonmoy.realtimemysql.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
---
+++ realtime mysql/src/main/java/com/tonmoy/realtimemysql/Xxx.java
@@ -0,0 +1,4 @@
+package com.tonmoy.realtimemysql;
+
+public class Xxx {
+}
